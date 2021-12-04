677f5e2df19ddb5f1cc9fdd2042e6830f377b0f0
yagizhan.deger <yagizhan.deger@d-teknoloji.com.tr>
2021-12-04 18:39:35 +0300

    yorum satırı eklendi

--- utils.dart
+++ utils.dart
@@ -12,53 +12,59 @@ var populationPyramide = [];
 var rnd = new Random();
 
 class Utils {
-  var newZealandPopulationPyramide = [19.431,64.201,16.368];
-  var turkeyPopulationPyramide = [23.942,67.077,8.981];
-  var indiaPopulationPyramide = [26.161,67.265,6.578];
+  var newZealandPopulationPyramide = [19.431, 64.201, 16.368];
+  var turkeyPopulationPyramide = [23.942, 67.077, 8.981];
+  var indiaPopulationPyramide = [26.161, 67.265, 6.578];
 
-  List getNewZealandPopulationPyramide(){
+  List getNewZealandPopulationPyramide() {
     print("Selected population: New Zealand");
     return newZealandPopulationPyramide;
   }
-  List getTurkeyPopulationPyramide(){
+
+  List getTurkeyPopulationPyramide() {
     print("Selected population: Turkey");
     return turkeyPopulationPyramide;
   }
-  List getIndiaPopulationPyramide(){
+
+  List getIndiaPopulationPyramide() {
     print("Selected population: India");
     return indiaPopulationPyramide;
   }
 
-  void createHumans(String selectedDifficulty){
-    switch(selectedDifficulty){
-      case "easy": {
-        populationPyramide = utils.getNewZealandPopulationPyramide();
-        break;
-      }
-      case "medium": {
-        populationPyramide = utils.getTurkeyPopulationPyramide();
-        break;
-      }
-      case "hard": {
-        populationPyramide = utils.getIndiaPopulationPyramide();
-      }
+  void createHumans(String selectedDifficulty) {
+    switch (selectedDifficulty) {
+      case "easy":
+        {
+          populationPyramide = utils.getNewZealandPopulationPyramide();
+          break;
+        }
+      case "medium":
+        {
+          populationPyramide = utils.getTurkeyPopulationPyramide();
+          break;
+        }
+      case "hard":
+        {
+          populationPyramide = utils.getIndiaPopulationPyramide();
+        }
     }
     int counter = 0;
-    while(counter < 100000){
-      if(counter<populationPyramide[0]*1000){
+    while (counter < 100000) {
+      if (counter < populationPyramide[0] * 1000) {
         int min = 0;
         int max = 14;
-        humans.add(new Human("Young"+counter.toString(),counter.toString(),min + rnd.nextInt(max - min)));
-      }
-      else if(counter<populationPyramide[1]*1000){
+        humans.add(new Human("Young" + counter.toString(), counter.toString(),
+            min + rnd.nextInt(max - min)));
+      } else if (counter < populationPyramide[1] * 1000) {
         int min = 15;
         int max = 64;
-        humans.add(new Human("Middle"+counter.toString(),counter.toString(),min + rnd.nextInt(max - min)));
-      }
-      else {
+        humans.add(new Human("Middle" + counter.toString(), counter.toString(),
+            min + rnd.nextInt(max - min)));
+      } else {
         int min = 65;
         int max = 100;
-        humans.add(new Human("Old"+counter.toString(),counter.toString(),min + rnd.nextInt(max - min)));
+        humans.add(new Human("Old" + counter.toString(), counter.toString(),
+            min + rnd.nextInt(max - min)));
       }
       counter++;
     }
@@ -67,8 +73,10 @@ class Utils {
     }*/
   }
 
-  void createFamilies(){
-    var families = new Families("1",[humans[1],humans[2],humans[2]]);
+  void createFamilies() {
+    var families = new Families("1", [humans[1], humans[2], humans[2]]);
     print(families.toString());
   }
+
+  //branch test
 }
