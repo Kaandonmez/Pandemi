047d5e116b887c8a42300cbd17219f6efeaea7ea
yagizhan.deger <yagizhan.deger@d-teknoloji.com.tr>
2021-12-12 18:33:33 +0300

    renamed and optimized

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'config.dart';
+
+void main() {
+  gameStarter();
+}
+void gameStarter() {
+  print(
+      "Please select game difficulty: " + difficultyOptions.toString());
+  setDifficulty(stdin.readLineSync()!);
+  utils.createHumans(difficulty); //!create hobbie bunun içinde bulunuyor.
+  utils.createFamilies();
+  utils.covidSpread();
+}
