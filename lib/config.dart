1a027a50ff5436da51311a8e8036e732ae7ef52f
yagizhan.deger <yagizhan.deger@d-teknoloji.com.tr>
2021-12-12 18:33:21 +0300

    options removed, fields moved to config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,10 +1,10 @@
 import 'dart:math';
 
-import 'options.dart';
 import 'utils.dart';
 
+String difficulty = "easy";
+List difficultyOptions = ["easy", "medium", "hard"];
 var utils = Utils();
-var options = Options();
 var humans = [];
 var families = [];
 var hobbies = [];
@@ -19,3 +19,13 @@ int cocukMin = 24700;
 int cocukMax = 99999;
 int yasliMin = 0;
 int yasliMax = 13899;
+
+void setDifficulty(String Difficulty) {
+  if (difficultyOptions.contains(Difficulty)) {
+    difficulty = Difficulty;
+    print("Selected difficulty is: " + difficulty);
+  } else {
+    print(
+        "You must select a value which is in difficulty options. Active difficulty is easy.");
+  }
+}
